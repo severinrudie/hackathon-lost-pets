f2c1cd24f8642bfe140ad8c5140ab16aede3a863
Baron-Severin <severinrudie@gmail.com>
2016-08-03 20:09:57 -0700

    extended models based on feedback

--- app/src/main/java/com/example/mgkan/hackathon_lost_pets/Model/DetailPet.java
+++ app/src/main/java/com/example/mgkan/hackathon_lost_pets/Model/DetailPet.java
@@ -5,18 +5,15 @@ package com.example.mgkan.hackathon_lost_pets.Model;
  */
 public class DetailPet extends ListPet {
 
-    String recordType;  // record_type  | values are FOUND / LOST / ADOPTABLE
     String link; // TODO: use URLEncoder and URLDecoder to maintain special characters
     int zip;  // zip
     String address;  // location_for_map_address |
     String memo;  //  memo | String broken up by <p></p>
 
-    public DetailPet(String animalId, Double[] coordinates, String animalType, String date,
-                     String dateType, String color, String age, String image, String city,
-                     String name, String recordType, String link, int zip, String address,
-                     String memo) {
-        super(animalId, coordinates, animalType, date, dateType, color, age, image, city, name);
-        this.recordType = recordType;
+    public DetailPet(String animalId, String animalType, String date, String dateType, String color,
+                     String age, String image, String city, String name, String gender, String breed,
+                     String link, int zip, String address, String memo) {
+        super(animalId, animalType, date, dateType, color, age, image, city, name, gender, breed);
         this.link = link;
         this.zip = zip;
         this.address = address;
--- app/src/main/java/com/example/mgkan/hackathon_lost_pets/Model/ListPet.java
+++ app/src/main/java/com/example/mgkan/hackathon_lost_pets/Model/ListPet.java
@@ -5,30 +5,33 @@ package com.example.mgkan.hackathon_lost_pets.Model;
  */
 public class ListPet {
 
+
     String animalId;  // animal_id  | the ID contains some letters
-    Double[] coordinates;  //  link | location_for_map.coordinates |
+//    double[] coordinates;  //  link | location_for_map.coordinates |
+    String animalBreed;  //  animal_breed |
     String animalType;  //  animal_type | 'Dog', 'Cat', 'Dead Bird', etc
     String date;  //  date | YYYY-MM-DD'T'tt:tt:tt.ttt
     //                   EG. 2016-08-03T00:00:00.000
     String dateType;  //  date_type | eg 'Received On'
     String color;  //  color |
-    String age;  //  age | eg 1 YEAR
     String image;  //  image | stored using URLEncode/URLDecode
     String city;  // location_for_map_city |
     String name;  // animal_name
+    String animalGender;  // animal_gender | 'Female' 'Male'
 
-    public ListPet(String animalId, Double[] coordinates, String animalType, String date,
-                   String dateType, String color, String age, String image, String city,
-                   String name) {
+    public ListPet(String animalId, String animalType, String date, String dateType, String color,
+                   String age, String image, String city, String name, String animalGender,
+                   String animalBreed) {
         this.animalId = animalId;
-        this.coordinates = coordinates;
+//        this.coordinates = coordinates;
         this.animalType = animalType;
         this.date = date;
         this.dateType = dateType;
         this.color = color;
-        this.age = age;
         this.image = image;
         this.city = city;
         this.name = name;
+        this.animalGender = animalGender;
+        this.animalBreed = animalBreed;
     }
 }
