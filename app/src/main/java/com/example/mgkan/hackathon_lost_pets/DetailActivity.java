92a6c874e588a26b41f98df1d5a3cade52635d94
MichaelKang <mgkang0206@gmail.com>
2016-08-04 14:07:28 -0700

    detail button gives info

--- app/src/main/java/com/example/mgkan/hackathon_lost_pets/DetailActivity.java
+++ app/src/main/java/com/example/mgkan/hackathon_lost_pets/DetailActivity.java
@@ -1,7 +1,12 @@
 package com.example.mgkan.hackathon_lost_pets;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,14 +17,21 @@ import com.koushikdutta.ion.Ion;
 
 import org.w3c.dom.Text;
 
-public class DetailActivity extends AppCompatActivity {
+import java.util.HashMap;
 
+public class DetailActivity extends AppCompatActivity {
+    public String address,animalId;
+    public HashMap<String,String> phoneNumber = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        getPhoneNumber();
         int pos = getIntent().getIntExtra("POS",0);
+        Pet currentAnimal = PetListActivity.pets.get(pos);
+        address = currentAnimal.getCurrentLocation();
+        animalId = currentAnimal.getAnimalId();
 
         Pet pet = PetListActivity.pets.get(pos);
         ImageView photo = (ImageView) findViewById(R.id.detailPhoto);
@@ -47,6 +59,7 @@ public class DetailActivity extends AppCompatActivity {
         TextView gender = (TextView) findViewById(R.id.dog_gender);
         TextView color = (TextView) findViewById(R.id.dog_color);
         TextView area = (TextView) findViewById(R.id.dog_area);
+        Button myPet = (Button) findViewById(R.id.myPetButton);
 
         breed.setText(pet.getAnimalBreed());
         date.setText(formattedDate);
@@ -54,9 +67,40 @@ public class DetailActivity extends AppCompatActivity {
         color.setText(pet.getColor());
         area.setText(pet.getCity());
 
+        myPet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog diaBox = showInfo();
+                diaBox.show();
+            }
+        });
+    }
+    private AlertDialog showInfo() {
+        AlertDialog myQuittingDialogBox =new AlertDialog.Builder(this)
+          //set message, title, and icon
+          .setTitle("Let's get your pet back!")
+          .setMessage("These are what you need to retrieve your pet.\n"
+            + "Address: "+ address+"\n"
+            + "Phone: "+ phoneNumber.get(address)+"\n"
+            + "Animal ID: "+ animalId)
+          .setIcon(R.drawable.ic_adb)
+
+          .create();
+        return myQuittingDialogBox;
 
     }
+    private HashMap<String,String> getPhoneNumber(){
+        phoneNumber.put("Covington Petco 27111 167TH PL SE COVINGTON, WA 98042","253-638-1064");
+        phoneNumber.put("Crossroads #204 Petco 15600 NE 8TH ST BELLEVUE, WA 98008","425-641-9333");
+        phoneNumber.put("In Public Home","206-296-2709");
+        phoneNumber.put("In RASKC Foster Home","206-296-7387");
+        phoneNumber.put("King County Pet Adoption Center 21615 64TH AVE S KENT, WA 98032","206-296-7388");
+        phoneNumber.put("Kirkland Petco 12040 NE 85TH ST KIRKLAND, WA 98033","425-889-8319");
+        phoneNumber.put("Meowtropolitan","206-632-2330");
+        phoneNumber.put("Reber Ranch 28606 132ND AVE SE KENT, WA 98042","253-630-3330");
 
+        return phoneNumber;
+    }
     private String monthConstructor(String month) {
         String result = "";
         switch (month) {
