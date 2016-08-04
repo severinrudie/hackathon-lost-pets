5005d1385ef7c4d19dfa04b0c6c1e7e5b52ea860
MichaelKang <mgkang0206@gmail.com>
2016-08-03 20:34:57 -0700

    query made

---
+++ app/src/main/java/com/example/mgkan/hackathon_lost_pets/Model/PetResponse.java
@@ -0,0 +1,51 @@
+package com.example.mgkan.hackathon_lost_pets.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by mgkan on 2016-08-03.
+ */
+public class PetResponse {
+  @SerializedName("page")
+  private int page;
+  @SerializedName("results")
+  private List<Pet> results;
+  @SerializedName("total_results")
+  private int totalResults;
+  @SerializedName("total_pages")
+  private int totalPages;
+
+  public int getPage() {
+    return page;
+  }
+
+  public void setPage(int page) {
+    this.page = page;
+  }
+
+  public List<com.example.mgkan.hackathon_lost_pets.Model.Pet> getResults() {
+    return results;
+  }
+
+  public void setResults(List<Pet> results) {
+    this.results = results;
+  }
+
+  public int getTotalResults() {
+    return totalResults;
+  }
+
+  public void setTotalResults(int totalResults) {
+    this.totalResults = totalResults;
+  }
+
+  public int getTotalPages() {
+    return totalPages;
+  }
+
+  public void setTotalPages(int totalPages) {
+    this.totalPages = totalPages;
+  }
+}
