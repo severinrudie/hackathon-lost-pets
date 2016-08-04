560bc06ed6932e730d4a31ddacfd7791192181b6
Matthew Duffin <matthewtduffin@gmail.com>
2016-08-04 04:45:47 -0700

    basic search logic

--- app/src/main/java/com/example/mgkan/hackathon_lost_pets/Activities/PetListActivity.java
+++ app/src/main/java/com/example/mgkan/hackathon_lost_pets/Activities/PetListActivity.java
@@ -6,6 +6,7 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mgkan.hackathon_lost_pets.Adapters.PetListAdapter;
 import com.example.mgkan.hackathon_lost_pets.Model.Pet;
@@ -52,6 +54,8 @@ public class PetListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_list);
 
+        handleIntent(getIntent());
+
         rvPets = (RecyclerView) findViewById(R.id.recyclerView_petList_petListActivity);
 
         type = getIntent().getStringExtra("TYPE");
@@ -83,6 +87,26 @@ public class PetListActivity extends AppCompatActivity {
                 searchManager.getSearchableInfo(getComponentName()));
 
         return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.i("MATTTEST","onNewIntent called");
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        Log.i("MATTTEST","handleIntent called");
+
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            populateWithSearchedFoundAnimal(rvPets,type,query);
+
+        }
+
     }
 
     private static final int REQUEST_PERMISSIONS = 1;
