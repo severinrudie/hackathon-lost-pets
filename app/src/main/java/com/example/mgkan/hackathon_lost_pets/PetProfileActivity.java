5b4a09bf8d4f7a700242dfb01f03816359a3329e
MichaelKang <mgkang0206@gmail.com>
2016-08-04 15:09:21 -0700

    lost my pet button added, petdetail dia box fixed

--- app/src/main/java/com/example/mgkan/hackathon_lost_pets/PetProfileActivity.java
+++ app/src/main/java/com/example/mgkan/hackathon_lost_pets/PetProfileActivity.java
@@ -1,7 +1,12 @@
 package com.example.mgkan.hackathon_lost_pets;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PetProfileActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class PetProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_profile);
+        final Button lostMyButton = (Button) findViewById(R.id.lostButton);
+
+        lostMyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.kingcounty.gov/depts/regional-animal-services/Lost-and-found/LOST/registerDog.aspx"));
+                startActivity(intent);
+            }
+        });
     }
 }
